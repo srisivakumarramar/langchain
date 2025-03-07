6565004177edd5a6ec2ccdd0fec641aecf1830e6
srisivakumarramar <sivakumarvelyampakkam@gmail.com>
2025-03-07 09:12:25 +0530

    Create vectordbFAISS.py

---
+++ vectordbFAISS.py
@@ -0,0 +1,24 @@
+# Job Listing using FAISS (facebook similarity) Vector to chuck the charcter from a document and to find matching job skill from input of the user with chroma 
+import os
+from langchain_ollama import OllamaEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS 
+
+
+llm = OllamaEmbeddings(model="llama3.2")
+
+document = TextLoader("job_listings.txt").load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+chunks = text_splitter.split_documents(document)
+db = FAISS.from_documents(chunks, llm)
+docs = retriever.invoke(text)
+text = input("Enter the query")
+for doc in docs:
+  print(doc.page_content)
+
+# another method  
+# retriever = db.as_retriever()
+# text = input("Enter the Query")
+# for doc in docs:
+#    print(doc.page_content)
